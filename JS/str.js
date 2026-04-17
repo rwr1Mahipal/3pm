578d0b2bb394e842bda1871855a211478eb3fa31
MAHIPAL SINGH <rwr1.mahipal.sb@gmail.com>
2026-04-17 15:34:35 +0530

    Add files via upload

---
+++ JS/str.js
@@ -0,0 +1,18 @@
+const str = "test12@gmail.com ";
+// const str1 = "jhgjhg"
+// const str2 = str.concat(str1)
+// const test = str.length
+// const test = str.trim()
+// const test = str.indexOf("@")
+// const test = str.charAt("10")
+// const test = str.replace("@", "#")
+// const test = str.includes("@")
+// const test = str.startsWith("T")
+// const test = str.split()
+// const test = str.slice(1,4)
+// const test = str.substring(0, 4) + "****";
+// const test = str.toUpperCase()
+const test = str.toLowerCase()
+
+
+console.log(test);
